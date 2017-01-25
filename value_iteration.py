995473f0db0f15f7c4966398ce209e31c0c0ea4a
Peter Henderson <breakend@wpa165080.wireless.mcgill.ca>
2017-01-25 10:37:31 -0500

    First pass at value iteration, using a lot of stuff from AI textbook

---
+++ value_iteration.py
@@ -0,0 +1,41 @@
+
+
+class ValueIteration:
+
+    def __init__(self, mdp):
+        self.mdp = mdp
+
+    def run(self, theta = 0.001):
+        # initialize array V arbitrarily
+        # V(s) = 0 for s in S
+        V = { s : 0.0 for s in self.mdp.states }
+
+        # Repeat
+        # TODO: turn this into vector form ??
+        iteration = 0
+        while True:
+            iteration += 1
+            delta = 0
+            for s in self.mdp.states:
+                v = V[s]
+                r = self.mdp.R(s)
+                action_possibilities = []
+                for a in self.mdp.possible_actions(s):
+                    action_possibilities.append(sum([p * (r + self.mdp.gamma*V[s_prime]) for (p, s_prime) in self.mdp.T(s,a)]))
+                V[s] = max(action_possibilities)
+                delta = max(delta, abs(v - V[s]))
+            if delta < theta:
+                break
+        print("Converged in %d iterations" % iteration)
+
+        pi = {}
+        for s in self.mdp.states:
+            action_possibilities = []
+            for a in self.mdp.possible_actions(s):
+                action_possibilities.append((a, sum([p * (r + self.mdp.gamma*V[s_prime]) for (p, s_prime) in self.mdp.T(s,a)])))
+            pi[s] = argmax(action_possibilities)
+
+        return pi
+
+def argmax(iterable):
+    return max(iterable, key=lambda x: x[1])[0]
