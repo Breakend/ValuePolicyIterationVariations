96b534852f6af604f3c4c0760a900b87c861c6fa
weidi-chang <chang.weidi@gmail.com>
2017-01-27 01:30:48 -0500

    Fixed modified PI and graphs

--- policy_iteration.py
+++ policy_iteration.py
@@ -1,13 +1,14 @@
 import numpy as np
 from operator import itemgetter
 
-def policy_iteration(mdp, gamma = 0.9, optimal_value = None):
+def policy_iteration(mdp, gamma = 0.9, epsilon = 0.01, optimal_value = None):
     # Initialization
     V = [0]*mdp.S
-    pol = [1]*mdp.S
+    pol = [0]*mdp.S
     old_V = V
     vs = []
     n_iter = 0
+    v_iter = 0
     while True:
         # Policy evaluation
         while True:
@@ -17,8 +18,9 @@ def policy_iteration(mdp, gamma = 0.9, optimal_value = None):
                 v = V[s]
                 V[s] = sum(mdp.T[s, pol[s], k] * (mdp.R[k] + gamma * V[k]) for k in range(mdp.S))
                 delta = max(delta, abs(v-V[s]))
-
-            if(delta < 0.001):
+            vs.append(np.linalg.norm(V - optimal_value))
+            v_iter+=1
+            if(delta < epsilon):
                 break
         # Policy improvement
         policy_stable = True
@@ -29,9 +31,8 @@ def policy_iteration(mdp, gamma = 0.9, optimal_value = None):
             if(old_action != pol[s]):
                 policy_stable = False
         if policy_stable:
-            print "Converged in %d iterations" % (n_iter)
+            print "Converged in %d iterations, %d evaluations" % (n_iter, v_iter)
             return V, pol, n_iter, vs
-        vs.append(np.linalg.norm(V - optimal_value))
         n_iter += 1
 
 def policy_iteration_by_inversion(mdp, gamma = 0.9, optimal_value = None):
@@ -41,6 +42,7 @@ def policy_iteration_by_inversion(mdp, gamma = 0.9, optimal_value = None):
     vs = []
     old_V = V
     n_iter = 0
+    v_iter = 0
     while True:
         # Policy evaluation
         T_new = np.zeros((mdp.S, mdp.S))
@@ -49,7 +51,7 @@ def policy_iteration_by_inversion(mdp, gamma = 0.9, optimal_value = None):
             T_new[s,:] = mdp.T[s, pol[s], :]
 
         V = np.array(np.linalg.inv(np.eye(mdp.S) - gamma*T_new).dot(mdp.R))
-
+        v_iter+=1
         vs.append(np.linalg.norm(V - optimal_value))
 
         # Policy improvement
@@ -60,54 +62,47 @@ def policy_iteration_by_inversion(mdp, gamma = 0.9, optimal_value = None):
             pol[s] = max(enumerate(possibilities), key=itemgetter(1))[0]
             if(old_action != pol[s]):
                 policy_stable = False
-        if policy_stable:
-            print "Converged in %d iterations" % (n_iter)
-            return V, pol, n_iter, vs
         n_iter += 1
+        if policy_stable:
+            print "Converged in %d iterations, %d evaluations" % (n_iter, v_iter)
+            return V, pol, n_iter, vs
 
-
-def modified_policy_iteration(mdp, epsilon = 0.001, gamma = 0.9, m = 20, optimal_value = None):
+def modified_policy_iteration(mdp, gamma = 0.9, epsilon = 1.0, m = 0, optimal_value=None):
     # Initialization
     V = [0]*mdp.S
     pol = [1]*mdp.S
+    old_V = V
     vs = []
     n_iter = 0
-
+    v_iter = 0
     while True:
-        u = []
-        u.append([])
-
         # Policy improvement
         policy_stable = True
         for s in range(mdp.S):
             old_action = pol[s]
             possibilities = [sum(mdp.T[s,a,k] *(mdp.R[k] + gamma * V[k]) for k in range(mdp.S)) for a in range(mdp.A)]
             pol[s] = max(enumerate(possibilities), key=itemgetter(1))[0]
-            u[0].append(max(possibilities))
+            if(old_action != pol[s]):
+                policy_stable = False
 
-        u.append([])
-        # Policy evaluation
-        i = 0
-        if np.linalg.norm(np.asarray(u[0]) - np.asarray(V)) < epsilon/(2*gamma):
-            print "Converged in %d iterations" % (n_iter)
+        if policy_stable:
+            print "Converged in %d iterations, %d evaluations" % (n_iter, v_iter)
             return V, pol, n_iter, vs
 
-        while True:
-            if (i == m):
-                V = u[i]
-                n_iter+=1
+        i = 0
+        # Policy evaluation
+        while i<m:
+            delta = 0.0
+            old_V = V
+            for s in range(mdp.S):
+                v = V[s]
+                V[s] = sum(mdp.T[s, pol[s], k] *
+                       (mdp.R[k] + gamma * V[k])
+                       for k in range(mdp.S))
+                delta = max(delta, abs(v-V[s]))
+            i += 1
+            vs.append(np.linalg.norm(V - optimal_value))
+            v_iter += 1
+            if delta < epsilon/(2*gamma):
                 break
-            else:
-                for s in range(mdp.S):
-                    u[i+1].append(sum(mdp.T[s, pol[s], k] * (mdp.R[k] + gamma * u[i][k]) for k in range(mdp.S)))
-                    u.append([])
-
-            if np.linalg.norm(np.asarray(u[i+1]) - np.asarray(u[i])) < epsilon/(2*gamma):
-                V = u[i]
-                n_iter+=1
-                break
-            else:
-                i+=1
-
-        vs.append(np.linalg.norm(V - optimal_value))
         n_iter += 1
